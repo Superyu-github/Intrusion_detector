02a773ed6c6feba989c26286ca5f4606359ee14f
Superyu <577928831@qq.com>
2020-08-26 18:57:13 +0800

    正常运行

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
@@ -60,16 +57,19 @@ data.iloc[:, -1] = LabelEncoder().fit_transform(data.iloc[:, -1])
 test.iloc[:, -1] = LabelEncoder().fit_transform(test.iloc[:, -1])
 data.iloc[:, 1:4] = OrdinalEncoder().fit_transform(data.iloc[:, 1:4])
 test.iloc[:, 1:4] = OrdinalEncoder().fit_transform(test.iloc[:, 1:4])
-# 划分训练集和测试集
+# 分离数据和标签
 x_data = data.drop(columns=['class'])
 y_data = data['class']
 x_test = test.drop(columns=['class'])
 y_test = test['class']
 # 划分用于训练的数据(只用了x/y_date_train,其他两个舍弃了)，此举用于控制用于训练的数据的数量，因总数据较多，个别模型训练耗时太长，
-# 此时可以调高test_size减少训练时间
+# 此时可以调高test_size节省训练时间
 x_data_train, x_data_test, y_data_train, y_data_test = train_test_split(x_data, y_data, test_size=0.1, random_state=1)
 # 将corrected分成两份，一份当作测试集调参，另一份当作“未知数据”用于展示最终效果
 x_test_1, x_test_2, y_test_1, y_test_2 = train_test_split(x_test, y_test, test_size=0.5, random_state=1)
+# 注：前期测试发现，按照常规步骤使用rain_test_split将x_data, y_data,划分为训练集和测试集，发现各项指标都极高（0.98左右），查阅资料发现，
+# 'corrected'文件中包含训练集中没有的未知攻击类型，更能模拟真实情况，于是后续步骤采用'corrected'文件中的数据进行调参和测试
+
 # 保存数据
 joblib.dump(x_data_train, 'save/x_data_train')
 joblib.dump(x_data_test, 'save/x_data_test')
@@ -80,8 +80,7 @@ joblib.dump(x_test_2, 'save/x_test_2')
 joblib.dump(y_test_1, 'save/y_test_1')
 joblib.dump(y_test_2, 'save/y_test_2')
 
-
-# 选择模型进行训练
+# 训练模型
 
 # 1.决策树模型
 from sklearn.tree import DecisionTreeClassifier
@@ -98,45 +97,45 @@ from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier(random_state=10, n_estimators=106, max_depth=31, n_jobs=-1)
 fit_model(model, 'RandomForestClassifier')
 
-# 4.高斯朴素贝叶斯模型
-from sklearn.naive_bayes import GaussianNB
-model = GaussianNB()
-fit_model(model, 'GaussianNB')
-
-# 5.多项式朴素贝叶斯模型
+# 4.多项式朴素贝叶斯模型
 from sklearn.naive_bayes import MultinomialNB
 model = MultinomialNB()
 fit_model(model, 'MultinomialNB')
 
-# 6.线性鉴别分析模型
+# 5.线性鉴别分析模型
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 model = LinearDiscriminantAnalysis()
 fit_model(model, 'LinearDiscriminantAnalysis')
 
-# 7.Quadratic Discriminant Analysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-model = QuadraticDiscriminantAnalysis(priors=1)
-fit_model(model, 'QuadraticDiscriminantAnalysis')
-
-# 8.逻辑回归模型
+# 6.逻辑回归模型
 from sklearn.linear_model import LogisticRegression
 model = LogisticRegression(max_iter=1200, n_jobs=-1)
 fit_model(model, 'LogisticRegression')
 
-# 9.GBDT(Gradient Boosting Decision Tree)
+# 7.GBDT(Gradient Boosting Decision Tree)
 from sklearn.ensemble import GradientBoostingClassifier
 model = GradientBoostingClassifier(learning_rate=0.01, max_depth=15, min_samples_leaf=80)
 fit_model(model, 'GradientBoostingClassifier')
 
-# 10.KNeighborsClassifier模型
+# 8.KNeighborsClassifier模型
 from sklearn.neighbors import KNeighborsClassifier
 model = KNeighborsClassifier(leaf_size=27, n_neighbors=4, n_jobs=-1)
 fit_model(model, 'KNeighborsClassifier')
 
-# 11.AdaBoost模型
+# 9.AdaBoost模型
 from sklearn.ensemble import AdaBoostClassifier
 model = AdaBoostClassifier(n_estimators=16)
 fit_model(model, 'AdaBoostClassifier')
+
+# 10.高斯朴素贝叶斯模型
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+fit_model(model, 'GaussianNB')
+
+# 11.Quadratic Discriminant Analysis
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+model = QuadraticDiscriminantAnalysis(priors=1)
+fit_model(model, 'QuadraticDiscriminantAnalysis')
 
 # 12.xgboost模型
 from xgboost import XGBClassifier
@@ -144,21 +143,21 @@ model = XGBClassifier(max_depth=3, subsample=0.8, colsample_bytree=0.8)
 fit_model(model, 'XGBClassifier')
 
 # 13.voting_classify
-from sklearn.ensemble import GradientBoostingClassifier, VotingClassifier, RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier, VotingClassifier, RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import MultinomialNB
 import xgboost
-from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 clf1 = GradientBoostingClassifier(n_estimators=200)
 clf2 = RandomForestClassifier(random_state=10, n_estimators=106, max_depth=31)
-# clf3 = LogisticRegression(random_state=1)
-clf4 = GaussianNB()
-# clf5 = xgboost.XGBClassifier()
+clf3 = AdaBoostClassifier(n_estimators=16)
+clf4 = MultinomialNB()
+clf5 = xgboost.XGBClassifier()
 model = VotingClassifier(estimators=[
-    # ('gbdt',clf1),
-    ('rf',clf2),
-    # ('lr',clf3),
-    ('nb',clf4),
-    # ('xgboost',clf5),
+    ('gbdt',clf1),
+    ('rf', clf2),
+    ('abc',clf3),
+    ('mnb', clf4),
+    ('xgboost',clf5),
     ],
     voting='soft',
     n_jobs=-1)
--- show.py
+++ show.py
@@ -3,7 +3,48 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
+import numpy as np
 import joblib
+
+
+# 自定义绘图函数1
+def show_picture_1(index):
+    y = np.arange(len(model_names)) + 1
+    x = np.array(list(dict[index]))
+    # 默认颜色表
+    color_list = ['c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c', 'c']
+    # 查找最大值，返回其索引，更改条形图颜色
+    max_index = dict[index].index(max(dict[index]))
+    color_list[max_index] = 'lightcoral'
+    # 绘制条形图
+    plt.barh(y - 0.2, x, height=0.5, tick_label=model_names, color=color_list)
+    plt.title(index)
+    # 设置数字标签
+    for a, b in zip(x, y):
+        plt.text(a + 0.06, b - 0.2, '%.4f' % a, fontsize=8, ha='right', va='center')
+    # 保存图片
+    plt.savefig(f'picture1/{index}.jpg', bbox_inches='tight', dpi=200)
+    # 清空画板
+    plt.cla()
+
+
+# 自定义绘图函数2
+def show_picture_2(index):
+    x_label = ['accuracy', 'precision', 'recall', 'f1']
+    x = np.array(x_label)
+    y = np.array(list(dict[index]))
+    # 绘制条形图
+    plt.bar(x, y, color=['lightcoral', 'mediumseagreen', 'darkorange', 'cornflowerblue'])
+    plt.title(index)
+    # 设置数字标签
+    for a, b in zip(x, y):
+        plt.text(a, b, '%.2f' % b, ha='center', va='bottom', fontsize=7)
+    # 保存图片
+    plt.savefig(f'picture2/{index}.jpg')
+    # 清空画板
+    plt.cla()
+
+
 # 加载数据
 x_data_train = joblib.load('save/x_data_train')
 x_data_test = joblib.load('save/x_data_test')
@@ -14,10 +55,10 @@ x_test_2 = joblib.load('save/x_test_2')
 y_test_1 = joblib.load('save/y_test_1')
 y_test_2 = joblib.load('save/y_test_2')
 
-model_names = ['DecisionTreeClassifier', 'LinearSVC', 'RandomForestClassifier', 'MultinomialNB',
+model_names = ('DecisionTreeClassifier', 'LinearSVC', 'RandomForestClassifier', 'MultinomialNB',
                'LinearDiscriminantAnalysis', 'LogisticRegression', 'GradientBoostingClassifier',
-               'AdaBoostClassifier', 'GaussianNB', 'QuadraticDiscriminantAnalysis','XGBClassifier',
-               'KNeighborsClassifier', 'VotingClassifier']
+               'KNeighborsClassifier', 'AdaBoostClassifier', 'GaussianNB', 'QuadraticDiscriminantAnalysis',
+               'XGBClassifier', 'VotingClassifier')
 dict = {}
 accuracy_list = []
 precision_list = []
@@ -28,16 +69,18 @@ for model_name in model_names:
     if model_name == 'KNeighborsClassifier':
         print(f'Loading {model_name}...')
         print(f'Testing {model_name}...')
+        # 注：因knn模型数据巨大，测试时间长，为节省展示时间，这里直接将预测好的数据储存，
+        # 展示时直接调用储存的数据，不在进行运算
         # model = joblib.load(f'save/{model_name}')
-        # predictions = model.predict(x_test_1)
-        # joblib.dump(predictions, 'save/knn_predictions')
-        predictions = joblib.load('save/knn_predictions')
+        # predictions = model.predict(x_test_2)
+        # joblib.dump(predictions, 'save/knn_predictions_test2')
+        predictions = joblib.load('save/knn_predictions_test2')
     else:
         print(f'Loading {model_name}...')
         print(f'Testing {model_name}...')
         model = joblib.load(f'save/{model_name}')
-        predictions = model.predict(x_test_1)
-    y = y_test_1
+        predictions = model.predict(x_test_2)
+    y = y_test_2
     accuracy = accuracy_score(y, predictions)
     precision = precision_score(y, predictions)
     recall = recall_score(y, predictions)
@@ -52,4 +95,21 @@ for model_name in model_names:
     dict['precision'] = precision_list
     dict['recall'] = recall_list
     dict['f1'] = f1_list
-print(dict['DecisionTreeClassifier'])
+
+# 开始画图
+
+# 第一种图 四个指标对应每个的13个模型
+
+# accuracy 图
+show_picture_1('accuracy')
+# precision 图
+show_picture_1('precision')
+# recall 图
+show_picture_1('recall')
+# f1 图
+show_picture_1('f1')
+
+# 第二种图 每个模型与其对应的四个指标
+
+for model_name in model_names:
+    show_picture_2(model_name)
